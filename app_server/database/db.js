b4a9fb2414d9fb1198f10e8683e4208aa352ddb5
Christian Rojas <rojaschristian96@gmail.com>
2023-01-31 12:07:19 -0600

    seedgoose updates

--- app_server/database/db.js
+++ app_server/database/db.js
@@ -3,42 +3,54 @@ const host = process.env.DB_HOST || '127.0.0.1'
 const dbURI = `mongodb://${host}/travlr`;
 const readline = require('readline');
 
-// avoid current server discovery and monitoring engine is deprecated
+//avoid current server discover and monitoring engine is deprecated
 mongoose.set('useUnifiedTopology', true);
 
 const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    }), 1000);
+  setTimeout(() => mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }), 1000);
 }
 
-mongoose.connection.on('connected', () => {            
-  console.log(`Mongoose connected to ${dbURI}`);       
-});                                                    
-mongoose.connection.on('error', err => {               
-  console.log('Mongoose connection error:', err);      
-});                                                    
-mongoose.connection.on('disconnected', () => {         
-  console.log('Mongoose disconnected');                
-});    
+//mongoose.connect(dbURI);
 
-process.once('SIGUSR2', () => {                          
-    gracefulShutdown('nodemon restart', () => {          
-      process.kill(process.pid, 'SIGUSR2');              
-    });
-  });
-  process.on('SIGINT', () => {                           
-    gracefulShutdown('app termination', () => {          
-      process.exit(0);                                   
-    });
-  });
-  process.on('SIGTERM', () => {                          
-    gracefulShutdown('Heroku app shutdown', () => {      
-      process.exit(0);                                   
-    });
-  });
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:', err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
 
-  connect();
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
 
-  require('./models/travlr');
+// For nodemon restarts                                 
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
+
+connect();
+
+require('./travlr');
