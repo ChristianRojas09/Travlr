848230a96158e569a52d49a409b49fb419be9cc5
Christian Rojas <rojaschristian96@gmail.com>
2023-02-14 17:40:06 -0600

    Created Angular SPA

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,7 +7,8 @@ const tripsController = require('../controllers/trips');
 //define a route to take a request for trips and pass it to the controller
 router
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 router //new route for tripCode
     .route('/trips/:tripCode')
